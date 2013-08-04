8be03c56cf860af0eb3d1673aafcff8deae0ebea
Keith Walbolt <keith.walbolt@gmail.com>
2013-08-04 18:57:18 -0400

    script now moves old pdfs to a folder

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 This script will take all the .pdf files in a directory and use
 [pdfocr](http://github.com/gkovacs/pdfocr) to run ocr and make them
-searchable pdfs. The raw files will be kept for now?
+searchable pdfs.
+
+Then, the old pdf's are moved to a directory called `obsolete pdfs` in case they are wanted, or the ocr'ing hoses them.
 
 ## usage
 
--- searchable.sh
+++ searchable.sh
@@ -3,8 +3,12 @@
 # [pdfocr](http://github.com/gkovacs/pdfocr) to run ocr and make them
 # searchable pdfs. The raw files will be kept for now?
 
+olddir="./obsolete pdfs"
+mkdir "$olddir"
+
 for raw in *.pdf
 do
 	justthename=`basename "$raw" | sed 's/\.pdf//'`
 	pdfocr -t -i "$raw" -o "$justthename.searchable.pdf"
+	mv "$raw" "$olddir/$justthename.`date "+%Y-%m-%d"`.pdf"
 done
